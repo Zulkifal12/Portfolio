aae0f50f4a2feb536391c3a5f101491e48f73a63
Muhammad Zulkifal <mzulkifaluetp@gmail.com>
2023-07-26 15:55:10 +0500

    Chnages done

--- src/app/services/language/language.service.ts
+++ src/app/services/language/language.service.ts
@@ -1,34 +1,33 @@
-import { Injectable } from '@angular/core';
-import { TranslateService } from '@ngx-translate/core';
-import { Location } from '@angular/common';
+import { Injectable } from "@angular/core";
+import { TranslateService } from "@ngx-translate/core";
+import { Location } from "@angular/common";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class LanguageService {
-
   language: "es" | "en";
 
   constructor(
     public translateService: TranslateService,
-    private location: Location,
+    private location: Location
   ) {}
 
-  initLanguage(){
-    this.translateService.addLangs(["en", "es"])
+  initLanguage() {
+    this.translateService.addLangs(["en", "es"]);
     let language = navigator.language || (navigator as any).userLanguage;
-    language = "en"
-    this.translateService.setDefaultLang(language)
+    language = "en";
+    this.translateService.setDefaultLang(language);
 
     // Change the URL without navigate:
-    this.location.go(language)
+    this.location.go(language);
 
-    this.language=language
+    this.language = language;
   }
 
-  changeLanguage(language){
-    this.translateService.setDefaultLang(language)
-    this.location.go(language)
-    this.language=language
+  changeLanguage(language) {
+    this.translateService.setDefaultLang(language);
+    this.location.go(language);
+    this.language = language;
   }
 }
